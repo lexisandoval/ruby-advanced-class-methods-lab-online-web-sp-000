2e8189fe90891cbe51de3f465a7df31e23943d72
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-26 17:06:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,6 +16,53 @@ class Song
     song
   end
 
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name 
+    song 
+  end
+
+  def self.create_by_name(name)
+    temp = self.create
+    temp.name = name 
+    temp 
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|el| el.name == name}
+  end
+    
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil 
+      self.create_by_name(name)
+    else 
+      self.find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    temp = @@all.sort_by { |song| song.name[0] }
+    temp
+  end
+
+  def self.new_from_filename(filename)
+    str = filename.split('-')
+    temp = self.new_by_name(str[1].gsub(".mp3", "").strip)
+    temp.artist_name = str[0].strip
+    temp
+  end
+
+  def self.create_from_filename(filename)
+    str = filename.split('-')
+    temp = self.create_by_name(str[1].gsub(".mp3", "").strip)
+    temp.artist_name = str[0].strip
+    temp
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
 
 
 end
